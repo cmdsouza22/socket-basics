a6aba0cc9542d66e72470110bcdfe667f76fe8e4
Cecilia DSouza <CeeDeeMac@Cecilias-MacBook-Air.local>
2016-05-20 17:55:47 -0400

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
 // all app js stored here 
+
+var  name = getQueryVariable('name') || 'Anonymous';
+var  room = getQueryVariable('room');
 var  socket = io(); 
+
+console.log(name + ' ' + 'wants to join ' + room); // currently manually entering into url then refresh 
+
 
 socket.on('connect', function(){
 	console.log('Connected to socket.io server');
@@ -9,12 +15,17 @@ socket.on('connect', function(){
 
 socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);  //after parsed as utc 
+	var $message = jQuery('.messages');      // to split it for multiple references
+
 	console.log('New Message:');
 	console.log(message.text); 
 
 // then call moment timestamp cancatenate to message.text
-	jQuery('.messages').append('<p><strong>'+ momentTimestamp.local().format('h:mm a: ') +'</strong>' + message.text +'</p>');  // to go with the div class (use .) in html when msg comein
+//	jQuery('.messages').append('<p><strong>'+ momentTimestamp.local().format('h:mm a: ') +'</strong>' + message.text +'</p>');  // to go with the div class (use .) in html when msg comein
+//	Split the above line into 2 for readbility 
 
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a:') + '</strong></p>');
+	$message.append('<p>' + message.text +'</p>');
 });
 // socket.emit('message',
 //	text: 'Welcome to the chat application! - app.js'
@@ -37,6 +48,7 @@ $form.on('submit', function (event){
 	var $message= $form.find('input[name=message]')	 //combine the 2 
 
 	socket.emit('message', {		// event + object (text in this case)
+		name: name,					// name var as in line1 
 		text: $message.val()	// set to value of input by calling method and find inside of message
 										// and search by attribute [] & pull value out and pass as a string 	});
 	}); 
